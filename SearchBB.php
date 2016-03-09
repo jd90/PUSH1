8818b3d11c1d2715185121a37b1a8f53a892ac84
jd90 <1418146@rgu.ac.uk>
2016-03-09 16:48:55 +0000

    fg

--- SearchBB.php
+++ SearchBB.php
@@ -309,7 +309,7 @@ NEWHTML;
         <img src="assets/glasgow.jpg" id="img2">
         <p>Glasgow, Scotland</p>
             <form action="searchResultsPage.php" method="POST">
-                <input hidden id='location' name="location" value='glasgow'>glasgow</input>
+                <input hidden name="location" value='glasgow'>glasgow</input>
                 <button type="submit" >Click to View.</button>
             </form>
         <p><li><strong><a href="#">click to view</a></strong></li></p>
