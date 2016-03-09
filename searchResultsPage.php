ed86f481c0bda65d811f3c5b914adbe7402b9131
jd90 <1418146@rgu.ac.uk>
2016-03-09 14:57:01 +0000

    fg

--- searchResultsPage.php
+++ searchResultsPage.php
@@ -192,7 +192,7 @@ $locations;
                 <<<NEWHTML
 
 
-                <form onsubmit="viewOnMap("{$row[longitude]},{$row[latitude]}")">
+                <form onSubmit="viewOnMap("{$row[longitude]},{$row[latitude]}")">
                     <div class="table4" id="{$count}">
 
     <p class="{$count}" id="bbname"><strong>{$row[bbname]}</strong></p>
