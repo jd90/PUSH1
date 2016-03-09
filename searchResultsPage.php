90b01e85e5cd0e2d0614687a4fce2fd45fcc5694
jd90 <1418146@rgu.ac.uk>
2016-03-09 13:15:34 +0000

    fg

--- searchResultsPage.php
+++ searchResultsPage.php
@@ -68,7 +68,7 @@ session_start();
         {
             var mapProp = {
                 center: new google.maps.LatLng(51.508742,-0.120850),
-                zoom:7,
+                zoom:10,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
             var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
