c84f0498204b0b1e2380d7d834484b62b6ac2b69
jd90 <1418146@rgu.ac.uk>
2016-03-17 18:05:53 +0000

    fg

--- B&Bregistration.php
+++ B&Bregistration.php
@@ -88,8 +88,8 @@ session_start();
                 <tr><td colspan="3"><p>* Required Fields</p></td>
 
                 <tr>
-                    <td><label hidden for="ownerid">Owner ID: *</label></td>
-                    <td><input hidden id="ownerid" type="text" class="inputform" name="ownerid" value="<?php echo"".$_SESSION['ownerid'];?>" size="20" maxlength="10" readonly /></td>
+                    <label hidden for="ownerid">Owner ID: *</label>
+                    <input hidden id="ownerid" type="text" class="inputform" name="ownerid" value="<?php echo"".$_SESSION['ownerid'];?>" size="20" maxlength="10" readonly />
                     <td><label for="region">Region: *</label></td>
                     <td><input id="region" type="text" class="inputform" name="region" placeholder="Enter Region" size="20" maxlength="30" required /></td>
                     <td><label for="bbdescription">B&B Description: *</label></td>
