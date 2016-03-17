93fa0be04757f6175f8adc67f44524dd3fb53744
jd90 <1418146@rgu.ac.uk>
2016-03-17 16:07:59 +0000

    fg

--- OwnerRegistration.php
+++ OwnerRegistration.php
@@ -70,80 +70,94 @@ session_start();
 </section>
 
 
-<section class="container" id="content2">
-    <form action="ownerRegistrationResultPage.php" method="post" id="form">
 
-        <table class="table1">
+        <section class="container" id="content2">
+            <form action="ownerRegistrationResultPage.php" method="post" id="form">
 
-            <tr><td class="smallfont"><p>* Required Fields</p></td></tr>
+                <table class="table1">
+
+                    <tr><td class="smallfont"><p>* Required Fields</p></td></tr>
+                    <tr><td>
+                            <label for="title">Title: *</label></td>
+                        <td><select class="inputform" id="title" name="title">
+                                <option value="">Select Title</option>
+                                <option value="Mr">Mr</option>
+                                <option value="Mrs">Mrs</option>
+                                <option value="Miss">Miss</option>
+                                <option value="Ms">Ms</option>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td><label for="firstname">First Name: *</label></td>
+                        <td><input type="text" class="inputform" id="firstname" name="firstname" placeholder="Enter your First Name" size="20" maxlength="25" required /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="surname">Surname: *</label></td>
+                        <td><input type="text" class="inputform" id="surname" name="surname" placeholder="Enter your Surname" size="20" maxlength="25" required /></td>
+                    </tr>
+
+                    <tr><td>
+                            <label for="address">Address: *</label></td>
+                        <td><input type="text" class="inputform" id="address" name="address" placeholder="Enter the first line of the Address" size="30" maxlength="50" required /></td>
+                    </tr>
+
+                    <tr><td>
+                            <label for="address2">Address Line 2: *</label></td>
+                        <td><input type="text" class="inputform" id="address2" name="address2" placeholder="Enter the second line of your Address" size="30" maxlength="50" required /></td>
+                    </tr>
+
+                    <tr><td>
+                            <label for="postcode">Postcode: *</label></td>
+                        <td><input type="text" class="inputform" id="postcode" name="postcode" placeholder="Enter the Postcode" size="20" maxlength="8" required /></td>
+                    </tr>
+
+                    <tr><td>
+                            <label for="city">City: *</label></td>
+                        <td><input type="text" class="inputform" id="city" name="city" placeholder="Enter City Name" size="20" maxlength="20" required /></td>
+                    </tr>
+
+                    <tr><td>
+                            <label for="telephone">Telephone: *</label></td>
+                        <td><input type="text" class="inputform" id="telephone" name="telephone" placeholder="Enter your Telephone Number" size="20" maxlength="20" required /></td>
+                    </tr>
+
+                    <tr><td>
+                            <label for="mobile">Mobile Telephone: *</label></td>
+                        <td><input type="text" class="inputform" id="mobile" name="mobile" placeholder="Enter your Mobile Telephone Number" size="20" maxlength="20" required /></td>
+                    </tr>
+
+                    <tr><td>
+                            <label for="email">Email: *</label></td>
+                        <td><input type="text" class="inputform" id="email" name="email" placeholder="Enter Email Address" size="20" maxlength="50" required /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="password">Password: *</label></td>
+                        <td>    <input type="password" class="inputform" id="password" name="password" placeholder="Enter a Password" size="20" maxlength="25" required>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td><label for="password2">Retype Password: *</label></td>
+                        <td><input type="password" class="inputform" id="password2" name="password2" placeholder="Enter Password again" size="20" maxlength="25" required>
+                        </td>
+                    </tr>
+
+                    <tr><td></td>
+                        <td><p><input id="submit" type="submit" value="Submit" class="submit" /></p></td>
+                    </tr>
+
+                    <tr><td></td>
+                        <td><a href="help.php#helpsection" id="textlink" target="_blank">need help?</a></td>
+                    </tr>
+                </table></form>
+
+        </section>
+        <section class="spacer" id="spacer">
 
 
-
-
-            <tr><td>
-                <label for="email">Email: *</label></td>
-                <td><input type="text" class="inputform" id="email" name="email" placeholder="email" size="30" maxlength="50" required /></td>
-            </tr>
-            <tr>
-                <td><label for="password">Password: *</label></td>
-                <td>    <input type="password" class="inputform" id="password" name="password" placeholder="enter password" required>
-                </td>
-            </tr>
-
-            <tr>
-                <td><label for="password2">Retype Password: *</label></td>
-                <td>    <input type="password" class="inputform" id="password2" name="password2" placeholder="enter password again" required>
-                </td>
-            </tr><tr><td>
-            <label for="title">Title: *</label></td>
-            <td><select class="inputform" id="title" name="title">
-                <option value="">Select Title</option>
-                <option value="Mr">Mr</option>
-                <option value="Mrs">Mrs</option>
-                <option value="Miss">Miss</option>
-                <option value="Ms">Ms</option>
-            </select>
-            </td>
-</tr>
-
-            <tr>
-                <td><label for="firstname">First Name: *</label></td>
-                <td><input type="text" class="inputform" id="firstname" name="firstname" placeholder="Enter your First Name" required /></td>
-            </tr>
-            <tr>
-                <td><label for="surname">Surname: *</label></td>
-                <td><input type="text" class="inputform" id="surname" name="surname" placeholder="Enter your Surname" required /></td>
-            </tr>
-            <tr>
-
-
-
-            <tr><td>
-                <label for="address">Address: *</label></td>
-                <td><input type="text" class="inputform" id="address" name="address" placeholder=" Enter first line of your address" size="30" maxlength="50" required /></td>
-            </tr>
-
-            <tr><td>
-                <label for="address2">Address Line 2: *</label></td>
-                <td><input type="text" class="inputform" id="address2" name="address2" placeholder=" Enter second line of your address" size="30" maxlength="50" required /></td>
-            </tr>
-
-
-            <tr><td>
-                <label for="telephone">Telephone: *</label></td>
-                <td><input type="text" class="inputform" id="telephone" name="telephone" placeholder=" Enter your telephone number" size="20" maxlength="20" required /></td>
-            </tr>
-
-            <tr><td></td>
-                <td><p><input id="submit" type="submit" value="Submit" class="submit" /></p></td>
-            </tr>
-        </table></form>
-
-</section>
-<section class="spacer" id="spacer">
-
-
-</section>
+        </section>
 <section class="container" id="foot">
 
     <div id="footernav">
